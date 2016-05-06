685de81685350e8724ca12c44c178af348fdf57c
foxxhot <hyhuyi@gmail.com>
2016-05-06 15:36:25 +0900

    Added files via upload

---
+++ deploy.ps1
@@ -0,0 +1,4 @@
+﻿$templatePath = "C:\Users\hu.yi\Desktop\Jsontest\tb\azure-deploy.json"
+$parameterPath = "C:\Users\hu.yi\Desktop\Jsontest\tb\parameter.json"
+$rgName = "tb-rg1"
+New-AzureRmResourceGroupDeployment -Name $rgName -ResourceGroupName $rgName -Mode Incremental -TemplateParameterFile $parameterPath -TemplateFile $templatePath -Verbose
